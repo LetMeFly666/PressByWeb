01a9baac2f8d74071dad119fd9e249fb10f762a6
LetMeFly666 <814114971@qq.com>
2024-01-31 21:33:05 +0800

    chore: 修改了按钮的功能(#1)
    将“增加音量”、“减小音量”快捷键修改为调整系统音量
    加上“静音按钮”

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 Author: LetMeFly
 Date: 2024-01-30 21:07:22
 LastEditors: LetMeFly
-LastEditTime: 2024-01-30 23:16:03
+LastEditTime: 2024-01-31 21:26:19
 '''
 from flask import Flask, request, render_template
 from functools import wraps
@@ -22,8 +22,9 @@ keyboard_mapping = {
     "Ctrl+Alt+F5": ("ctrl", "alt", "f5"),
     "Ctrl+Alt+→": ("ctrl", "alt", "right"),
     "Ctrl+Alt+←": ("ctrl", "alt", "left"),
-    "Ctrl+Alt+↑": ("ctrl", "alt", "up"),
-    "Ctrl+Alt+↓": ("ctrl", "alt", "down")
+    "volumeup": ("volumeup", ),  # 括号里的逗号必须要写，否则将被识别为字符串而不是元组
+    "volumedown": ("volumedown", ),
+    "volumemute": ("volumemute", )
 }
 
 
@@ -31,7 +32,6 @@ def authChecker(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         thisKey = request.cookies.get('key')
-        print(thisKey)
         if thisKey != PASSWORD:
             return """<body><input id="keyInput" type="text" placeholder="Enter key value" onkeydown="if(event.keyCode === 13) { document.cookie = 'key=' + this.value + ';path=/;expires=' + new Date(new Date().getTime() + 86400000 * 365 * 20).toUTCString(); location.reload(); }" value=""><button onclick="document.cookie = 'key=' + document.getElementById('keyInput').value + ';path=/;expires=' + new Date(new Date().getTime() + 86400000 * 365 * 20).toUTCString(); location.reload();">设置 Cookie 并刷新</button><script>document.getElementById('keyInput').value = (document.cookie.match('(^|;) ?key=([^;]*)(;|$)') || [])[2] || '';</script></body>"""
         return func(*args, **kwargs)
@@ -50,6 +50,7 @@ def simulate_keypress():
     key_combination = request.json.get('key')
     if key_combination in keyboard_mapping:
         key_sequence = keyboard_mapping[key_combination]
+        print(*key_sequence)
         pyautogui.hotkey(*key_sequence)  # 解包键序列
         return '快捷键按键成功'
     else:
